c3047223511a821ce6b8bf79626e351c3ce54422
Hassan Alharbi <Hassan Alharbi>
2017-09-16 17:26:10 +0300

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0, y = 0;
+            double result = 0;
+
+            Console.WriteLine("Please Enter The First Number:");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please Enter The Second Number:");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            result = x + y;
+
+            Console.WriteLine("The sum = " + result);
+            Console.ReadKey();
+        }
+    }
+}
